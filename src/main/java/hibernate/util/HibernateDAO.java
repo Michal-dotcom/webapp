6147f01fee9ff053add6b8e185ad09fcba84b909
Jeff <michalwaszkiewicz1996@gmail.com>
2020-10-03 12:44:28 +0200

    commit

---
+++ src/main/java/hibernate/util/HibernateDAO.java
@@ -0,0 +1,13 @@
+package hibernate.util;
+
+public interface HibernateDAO {
+    //CRUD
+
+
+    public void create();
+    public void read();
+    public void update();
+    public void delete();
+
+
+}
